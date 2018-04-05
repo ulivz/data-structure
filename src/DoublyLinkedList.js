7cb947b4a2308662abcaac919b5986f165f6ae28
ULIVZ <472590061@qq.com>
2018-04-05 22:31:15 +0800

    finished DoublyLinkedList

--- src/DoublyLinkedList.js
+++ src/DoublyLinkedList.js
@@ -75,6 +75,108 @@ export default class DoublyLinkedList {
   }
 
   /**
+   * Remove an item from the specific location of the list.
+   * @param {number} position
+   * @returns {any} the deleted element
+   */
+  removeAt(position) {
+    if (position < 0 || position >= this.length) return null
+
+    let current = this.head
+    if (position === 0) {
+      if (this.length == 1) {
+        this.head = null
+        this.tail = null
+      } else {
+        this.head = this.head.next
+        this.head.prev = null
+      }
+
+    } else if (position === this.length - 1) {
+      current = this.tail
+      this.tail = this.tail.prev
+      this.tail.next = null
+
+    } else {
+      let index = 0
+      let previous = null
+
+      while (index++ < position) {
+        previous = current
+        current = current.next
+      }
+
+      previous.next = current.next
+      current.next.prev = previous
+    }
+
+    this.length--
+    return current.element
+  }
+
+  /**
+   * Remove an item from the lists.
+   * @param {any} element
+   * @returns {any} the deleted element
+   */
+  remove(element) {
+    const position = this.indexOf(element)
+    return this.removeAt(position)
+  }
+
+  /**
+   * Returns the index of the element in the list. If the element.
+   * does not exist in the list, it returns -1.
+   * @param {any} element
+   * @returns {number} index
+   */
+  indexOf(element) {
+    let current = this.head
+    let index = 0
+    while (current) {
+      if (current.element === element) {
+        return index
+      }
+      index++
+      current = current.next
+    }
+    return -1
+  }
+
+  /**
+   * If the list does not contain any elements, it returns to true.
+   * If the length of the linked list is greater than 0, it returns false.
+   * @returns {boolean} is empty
+   */
+  isEmpty() {
+    return this.length === 0
+  }
+
+  /**
+   * Returns the number of elements contained in the list.
+   * @returns {number} size
+   */
+  size() {
+    return this.length
+  }
+
+  /**
+   * Get first element from list.
+   * @returns {any} element
+   */
+  getHead() {
+    return this.head.element
+  }
+
+  /**
+   * Get last element from list.
+   * @returns {any} element
+   */
+  getTail() {
+    return this.tail.element
+  }
+
+  /**
    * Clean current list's data.
    */
   clear() {
--- test/DoublyLinkedList.test.js
+++ test/DoublyLinkedList.test.js
@@ -16,6 +16,10 @@ test('DoublyLinkedList', () => {
   expect(dll.toString()).toBe('1,2,3')
   expect(dll.forwardString()).toBe('1,2,3')
   expect(dll.reverseString()).toBe('3,2,1')
+  expect(dll.size()).toBe(3)
+  expect(dll.indexOf(2)).toBe(1)
+  expect(dll.getHead()).toBe(1)
+  expect(dll.getTail()).toBe(3)
 })
 
 test('DoublyLinkedList - insert', () => {
@@ -28,4 +32,20 @@ test('DoublyLinkedList - insert', () => {
 
   dll.insert(5, 5)
   expect(dll.toString()).toBe('0,1,4,2,3,5')
+})
+
+test('DoublyLinkedList - removeAt', () => {
+  dll.removeAt(1)
+  expect(dll.toString()).toBe('1,3')
+
+  dll.removeAt(-1)
+  expect(dll.toString()).toBe('1,3')
+
+  dll.removeAt(-2)
+  expect(dll.toString()).toBe('1,3')
+})
+
+test('DoublyLinkedList - remove', () => {
+  dll.remove(2)
+  expect(dll.toString()).toBe('1,3')
 })
