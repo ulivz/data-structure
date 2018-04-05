0708568fa356217cbf02b0bb6700e037470daccd
ULIVZ <472590061@qq.com>
2018-04-05 23:11:35 +0800

    refactor code

--- src/Set.js
+++ src/Set.js
@@ -3,29 +3,55 @@ export default class Set {
     this.elements = {}
   }
 
+  /**
+   * Check if a value is existed in this set.
+   * @param {string|number} value
+   * @returns {boolean}
+   */
   has(value) {
     return this.elements.hasOwnProperty(value)
   }
 
+  /**
+   * Add a value in this set.
+   * @param {string|number} value
+   * @returns {boolean}
+   */
   add(value) {
     if (this.has(value)) return false
     this.elements[value] = value
     return true
   }
 
+  /**
+   * Remove a value from this set.
+   * @param {string|number} value
+   * @returns {boolean}
+   */
   remove(value) {
     if (!this.has(value)) return false
     delete this.elements[value]
   }
 
+  /**
+   * Clear this set.
+   */
   clear() {
     this.elements = {}
   }
 
+  /**
+   * Get this set's length.
+   * @returns {number}
+   */
   size() {
     return this.values().length
   }
 
+  /**
+   * Get this set's values.
+   * @returns {Array}
+   */
   values() {
     return Object.keys(this.elements)
   }
